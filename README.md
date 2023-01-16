348675e76849163f528053a2010219bcd4eca95c
Morten Sørvig <morten.sorvig@qt.io>
2023-01-16 08:58:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+## Qt for WebAssembly development server. 
+
+This server is intented to be used while developing and testing WebAssembly applications on a
+trusted network. It is not suitable for production use, i.e. distributing applicaitons to end
+users over public networks.
+
+The server is an upgrade from the python one line server ("python -m http.server"), and offers the
+following features:
+
+* Zero-configuration / minimal configuration.
+
+* Support for binding to multiple addresses in addition to localhost. This is useful
+  for e.g. testing on mobile devices on the same local network as the development machine.
+  Address can be addded individually using the --address option. Pass the "-all" option to
+  bind to all available addresses.
+
+* Support for generating https certificates using the mkcert utility. Many web features
+  require a secure context. While "localhost" is considered a secure context (also when plain
+  http is used), other addresses are not. Clicking through the "not secure" warnings is
+  an option, but using a valid certificate improves flow.
+
+* Support for compression using brotli. By default, the server compresses when serving over a
+  public, non-localhost address. (localhost is fast, enabling compression here usualually increases
+  download time instead of the opposite). Compression be controlled with the --compress-always and 
+  --compress-never options.
+
+* Support for enabling site isolation mode. This sets the so-called COOP and COEP headers,
+  which are required to enable SharedArrayBuffer and multithreading. Enable with the --site-isolation option.
+  While there is little reason to not enable SharedArrayBuffer, site isolation mode also enables
+  additional restrictions for cross origin requests, which may come as a surprise.
+
+It also has a couple of non-features:
+
+* Support for starting/stopping web browsers.
+
+* Support for forwarding standard output to the console.
+
+If you are looking to use features like the above, then emrun from Emscripten is a better choice.
+
+TODO:
+
+ * ipv6 support
+
+# Installation and Usage
+
+pip install qtwasmserver
+
+qtwasmserver                        # Start server on localhost, serve $CWD
+qtwasmserver /path/to/wasm/builds   # Specify web root path
+qtwasmserver --all-interfaces       # Start server(s) on all network interfaces
+qtwasmserver -a 10.0.0.2            # Start server on specific address, in addition to localhost
+qtwasmserver --site-isolation       # Enable site isolation mode for multithreading
+qtwasmserver -h                     # Show help
+
+# Using mkcert
+
+qtwasmserver can optinally use mkcert to generate https certifacates. See https://github.com/FiloSottile/mkcert for
+installation and getting started instructions.
+
+The basic flow is:
+
+ 1. Gereate a certificate authority (CA), and install that on all devices and browsers.
+    This is a one time operation. mkcert will use thuis CA to sign certificates.
+ 2. Generate a certificate for each address you want to use. This is done automatically
+    by this server.
+
+The main beneifits of this appraoch is that certificates can be freely generated if/when
+the network address for the server changes, for example when moving between home and office
+networks, and that it does not requie reqeusting certificates from a third party.
---
+++ src/qtwasmserver/qtwasmserver.py
@@ -0,0 +1,282 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import pathlib
+import socket
+import ssl
+import subprocess
+import sys
+import tempfile
+import threading
+from enum import Enum
+from http import HTTPStatus
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
+from subprocess import run
+
+import brotli
+import netifaces as ni
+from httpcompressionserver import HTTPCompressionRequestHandler
+
+def generate_mkcert_certificate(addresses):
+    """ "Generates a https certificate for localhost and selected addresses. This
+    requires that the mkcert utility is installed, and that a certificate
+    authority key pair (rootCA-key.pem and rootCA.pem) has been generated. The
+    certificates are written to /tmp, where the http server can find them
+    ater on."""
+
+    cert_file = tempfile.NamedTemporaryFile(
+        mode="w+b", prefix="qtwasmserver-certificate-", suffix=".pem", delete=True
+    )
+    cert_key_file = tempfile.NamedTemporaryFile(
+        mode="w+b", prefix="qtwasmserver-certificate-key-", suffix=".pem", delete=True
+    )
+
+    # check if mkcert is installed
+    try:
+        out = subprocess.check_output(["mkcert", "-CAROOT"])
+        root_ca_path = out.decode("utf-8").strip()
+        print(
+            "Generating certificates with mkcert, using certificate authority files at:"
+        )
+        print(f"   {root_ca_path}\n")
+    except Exception as e:
+        print("Warning: Unable to run mkcert. Will not start HTTPS server")
+        print(e)
+        return False
+
+    # generate certificates
+    addresses_string = f"localhost {' '.join(addresses)}"
+    ret = run(
+        f"mkcert -cert-file {cert_file.name} -key-file {cert_key_file.name} {addresses_string}",
+        shell=True,
+    )
+    has_certificate = ret.returncode == 0
+    if not has_certificate:
+        print(
+            "Warning: mkcert is not installed or was unable to create a certificate. Will not start HTTPS server."
+        )
+    return has_certificate, cert_file, cert_key_file
+
+
+def send_site_isolation_headers(handler):
+    """Sends COOP and COEP site isolation headers"""
+    handler.send_header("Cross-Origin-Opener-Policy", "same-origin")
+    handler.send_header("Cross-Origin-Embedder-Policy", "require-corp")
+    handler.send_header("Cross-Origin-Resource-Policy", "cross-origin")
+
+
+def send_empty_favicon(handle):
+    """Sends an empty icon to surpess missing faviocon errors"""
+    self.send_response(200)
+    self.send_header("Content-Type", "image/x-icon")
+    self.send_header("Content-Length", 0)
+
+
+class HttpRequestHandler(SimpleHTTPRequestHandler):
+    protocol_version = "HTTP/1.1"
+
+    def end_headers(self):
+        if self.site_isolation == True:
+            send_site_isolation_headers(self)
+        super().end_headers()
+
+
+class CompressionHttpRequesthandler(HTTPCompressionRequestHandler):
+    protocol_version = "HTTP/1.1"
+
+    # Add wasm to the list of compressed types.
+    compressed_types = HTTPCompressionRequestHandler.compressed_types.copy()
+    compressed_types.append("application/wasm")
+
+    def brotli_producer(fileobj):
+        bufsize = 1024 * 512
+
+        # Create compressor with quallity such that applying compression brings an actual speedup,
+        # i.e. the server must compress fast enough to saturate a typical network
+        # connection with compressed data. For brotli the quality goes from 0 to 11.
+        compressor = brotli.Compressor(quality=2)
+        with fileobj:
+            while True:
+                buf = fileobj.read(bufsize)
+                if not buf:
+                    yield compressor.finish()
+                    return
+                yield compressor.compress(buf)
+
+                # must flush compressor state to work around crash/assert in brotlicffi,
+                # see https://github.com/python-hyper/brotlicffi/issues/167
+                buf = compressor.flush()
+                if len(buf) > 0:
+                    yield buf
+
+    # Ideally, we would like to use the default gzip/deflate support as well,
+    # however that makes gzip take precedence over brotli. In practice, all
+    # browsers support brotli, so we can just use that.
+    compressions = {}  # HTTPCompressionRequestHandler.compressions.copy()
+    compressions["br"] = brotli_producer
+
+    def end_headers(self):
+        if self.site_isolation == True:
+            send_site_isolation_headers(self)
+        super().end_headers()
+
+
+class CompressionMode(Enum):
+    AUTO = 1
+    ALWAYS = 2
+    NEVER = 3
+
+
+def select_http_handler_class(compression_mode, address):
+    """Returns the http handler class to use, based on the compression mode,
+    and the address of the server for the auto mode."""
+    if compression_mode == CompressionMode.ALWAYS:
+        return CompressionHttpRequesthandler
+    elif compression_mode == CompressionMode.NEVER:
+        return HttpRequestHandler
+    else:
+        # Select http request handler based on addrees. If the address is
+        # localhost then compression is typically not worth it since the
+        # localhost connection is very fast. For other addresses we assume
+        # normal network bandwidth and enable compression to reduce the download
+        # size.
+        if address == "127.0.0.1":
+            return HttpRequestHandler
+        else:
+            return CompressionHttpRequesthandler
+
+
+# Serve cwd from http(s)://address:port, with certificates from certdir if set
+def serve_on_thread(
+    address, port, secure, cert_file, cert_key_file, compression_mode, site_isolation
+):
+    handler = select_http_handler_class(compression_mode, address)
+    handler.site_isolation = site_isolation
+    httpd = ThreadingHTTPServer((address, port), handler)
+    if secure:
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        context.load_cert_chain(cert_file.name, cert_key_file.name)
+        httpd.socket = context.wrap_socket(
+            httpd.socket,
+            server_side=True,
+        )
+    thread = threading.Thread(target=httpd.serve_forever)
+    thread.start()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="A development web server for Qt for WebAssembly applications.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--port",
+        "-p",
+        help="Port on which to listen for HTTP and HTTPS (PORT + 1)",
+        type=int,
+        default=8000,
+    )
+    parser.add_argument(
+        "--address",
+        "-a",
+        help="Bind to additional address, in addition to localhost",
+        action="append",
+    )
+    parser.add_argument(
+        "--all-interfaces",
+        help="Bind to all local interfaces, instead of locahost only",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--site-isolation",
+        help="Enables site isolation mode, required for WebAssembly threads",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--compress-auto",
+        help="Enables file compression on non-localhost addresses",
+        action="store_true",
+        default=True,
+    )
+    parser.add_argument(
+        "--compress-always",
+        help="Enables file compression for all addresses",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--compress-never",
+        help="Disables file compression",
+        action="store_true",
+    )
+    parser.add_argument(
+        "path", help="The directory to serve", nargs="?", default=os.getcwd()
+    )
+
+    args = parser.parse_args()
+    http_port = args.port
+    https_port = http_port + 1
+    all_interfaces = args.all_interfaces
+    cmd_addresses = args.address or []
+    serve_path = args.path
+    site_isolation = args.site_isolation
+
+    compression_mode = CompressionMode.AUTO
+    if args.compress_always:
+        compression_mode = CompressionMode.ALWAYS
+    elif args.compress_never:
+        compression_mode = CompressionMode.NEVER
+
+    print("Starting Qt for WebAssembly development server.\n")
+    print(f"Web server root:\n  {serve_path}\n")
+
+    addresses = ["127.0.0.1"] + cmd_addresses
+    if all_interfaces:
+        addresses += [
+            addr[ni.AF_INET][0]["addr"]
+            for addr in map(ni.ifaddresses, ni.interfaces())
+            if ni.AF_INET in addr
+        ]
+    addresses = list(set(addresses))  # deduplicate
+    addresses.sort()
+
+    print("Serving at addresses:")
+    print(f"   {addresses}\n")
+
+    has_certificate, cert_file, cert_key_file = generate_mkcert_certificate(addresses)
+
+    print("Options:")
+    print(f"   Secure server:          {has_certificate}")
+    print(f"   Site isolation mode:    {site_isolation}")
+    print(f"   Compression:            {compression_mode}")
+    print("")
+
+    # Start servers
+    print(f"Serving at:")
+    for address in addresses:
+        print(f"    http://{address}:{http_port}")
+        serve_on_thread(
+            address,
+            http_port,
+            False,
+            cert_file,
+            cert_key_file,
+            compression_mode,
+            site_isolation,
+        )
+
+    if has_certificate:
+        for address in addresses:
+            print(f"    https://{address}:{https_port}")
+            serve_on_thread(
+                address,
+                https_port,
+                True,
+                cert_file,
+                cert_key_file,
+                compression_mode,
+                site_isolation,
+            )
+
+
+if __name__ == "__main__":
+    main()
